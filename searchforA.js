193ef4312e77e4e9845ae881fbb5e2333bff7793
Quanda Grant <quandagrant@gmail.com>
2026-01-11 19:48:59 -0500

    added more excerise

---
+++ searchforA.js
@@ -0,0 +1,3 @@
+let searchforA = ["apple", "banana", "cherry", "date", "apricot", "grape"];
+const filteredFruits = searchforA.filter(fruit => fruit.startsWith("a"));
+console.log(filteredFruits);
---
+++ servers.js
@@ -0,0 +1,8 @@
+let servers = [200, 404, 500].forEach(code => `Status received: ${code}`);
+console.log(servers);
+// expected output:
+// Status received: 200
+// Status received: 404
+// Status received: 500
+
+
